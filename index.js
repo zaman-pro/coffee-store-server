035f2a6422d484b341f65cc6c34d0fab9b6f6921
unknown <zaman-pro>
2025-05-18 19:44:03 +0600

    users get API added

--- index.js
+++ index.js
@@ -76,6 +76,12 @@ async function run() {
     });
 
     // users related APIs
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const userProfile = req.body;
       const result = await usersCollection.insertOne(userProfile);
